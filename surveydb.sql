83ea798b4d50ee991323eb0ed9067f1da9ecf523
aliakhtarmdh <87354793+aliakhtarmdh@users.noreply.github.com>
2024-08-12 09:42:44 +0800

    updated db

--- surveydb.sql
+++ surveydb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 09, 2024 at 02:44 AM
+-- Generation Time: Aug 12, 2024 at 01:42 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.2.20
 
@@ -32,12 +32,20 @@ CREATE TABLE IF NOT EXISTS `surveys` (
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `age` int DEFAULT NULL,
+  `document` longtext,
   `feedback_type` varchar(50) DEFAULT NULL,
   `services_used` text,
   `additional_comments` text,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `surveys`
+--
+
+INSERT INTO `surveys` (`id`, `name`, `email`, `age`, `document`, `feedback_type`, `services_used`, `additional_comments`) VALUES
+(5, 'test', 'ali@gmail.com', 24, 'uploads/team-collaboration-postmanaut_4BKG-1200x817.png', 'negative', 'Service2,Service3', '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
